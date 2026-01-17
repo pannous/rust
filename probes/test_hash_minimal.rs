1b5975fee52ffdd4e8324394e9bd98d7c6960a6f
pannous <info@pannous.com>
2026-01-17 12:52:08 +0100

    test: Update test_hash_minimal.rs for hash indexing

--- probes/test_hash_minimal.rs
+++ probes/test_hash_minimal.rs
@@ -1,4 +1,17 @@
 #!/usr/bin/env rust
 
-z := @['a', 'b', 'c']
-eq!( z#1 , 'a');
+z := ['a', 'b', 'c']
+assert_eq!(z#1, 'a')
+assert_eq!(z#2, 'b')
+assert_eq!(z#3, 'c')
+
+// Variable index
+i := 2
+assert_eq!(z#i, 'b')
+
+// Chained
+matrix := [[1, 2], [3, 4]]
+assert_eq!(matrix#1#1, 1)
+assert_eq!(matrix#2#2, 4)
+
+put!("hash indexing works!")
